917a84f790f3d5e4591f9b1dadd7def6aecdf775
playerarm123 <anuwat_sukhsa@outlook.com>
2025-07-25 15:50:35 +0700

    fix: add missing function to FakeSms

--- src/Providers/FakeSms.php
+++ src/Providers/FakeSms.php
@@ -5,6 +5,7 @@ namespace Mesalution\LaravelMesms\Providers;
 use Illuminate\Support\Str;
 use Mesalution\LaravelMesms\Data\Otp;
 use Mesalution\LaravelMesms\Contracts\Sms;
+use Mesalution\LaravelMesms\Exceptions\InvalidOtpException;
 
 class FakeSms implements Sms
 {
@@ -16,5 +17,26 @@ class FakeSms implements Sms
             "OTP Code Is: " . $this->validOtp,
         );
         return $otp;
+    }
+    public function resendOTP(string $otpId): Otp
+    {
+        $otp = new Otp(
+            Str::random(),
+            "OTP Code Is: " . $this->validOtp,
+        );
+        return $otp;
+    }
+    public function verifyOTP(string $otpId, string $otpCode): void
+    {
+        if ($otpCode != $this->validOtp) {
+            throw new InvalidOtpException(
+                provider: class_basename($this),
+                context: [
+                    'otpId' => $otpId,
+                    'otpCode' => $otpCode,
+                    'verifyData' => [],
+                ]
+            );
+        }
     }
 }
